043ba50f294d747edddb96a40b9a1fade500a75f
Madhawa-Tharindu <madhawath@gmail.com>
2024-09-01 18:42:45 +0530

    implemeted the socket for real-time messaging

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-//import { app, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const app = express();  // Add this line to initialize `app`
+//const app = express();  // Add this line to initialize `app`
 
-//const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -24,11 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
 	connectToMongoDB();
